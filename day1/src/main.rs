27723af35178f2a5fd791d39b5178347356b5ac1
Kabir Sala <kabisala@amazon.com>
2020-12-02 08:59:18 +0000

    Remove useless vec && cargo fmt

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -12,36 +12,31 @@ fn main() {
 const YEAR: i32 = 2020;
 
 fn hashmap_solution() {
-    let nums: Vec<i32> = input::to_vec("src/input.txt", "\n");
     let hashnums: HashSet<i32> = input::to_set("src/input.txt", "\n");
     let mut found_two = false;
     let mut found_three = false;
-    for i in 0..nums.len() {
+    for (i, a) in hashnums.iter().enumerate() {
         // Sum of 2 numbers == 2020
-        let a = 2020-nums.get(i).unwrap();
-        match hashnums.get(&(YEAR-a)) {
-            None => {},
-            Some(b) => { 
+        match hashnums.get(&(YEAR - a)) {
+            None => {}
+            Some(b) => {
                 if !found_two {
                     found_two = true;
                     println!("{}, {}, {}", a, b, a * b);
                 }
-            },
+            }
         }
 
-        // Sum of 3 numbers == 2020
-        for j in (i+1)..nums.len() {
-            let a = nums.get(i).unwrap();
-            let b = nums.get(j).unwrap();
+        for b in hashnums.iter().skip(i) {
             if a + b < YEAR {
-                match hashnums.get(&(YEAR-(a+b))) {
-                    None => {},
-                    Some(c) => { 
+                match hashnums.get(&(YEAR - (a + b))) {
+                    None => {}
+                    Some(c) => {
                         if !found_three {
                             found_three = true;
                             println!("{}, {}, {}, {}", a, b, c, a * b * c);
                         }
-                    },
+                    }
                 }
             }
         }
@@ -52,7 +47,7 @@ fn loop_solution() {
     let nums: Vec<i32> = input::to_vec("src/input.txt", "\n");
     for x in 0..nums.len() {
         // We can start at x to avoid processing numbers already processed
-        for y in (x+1)..nums.len() {
+        for y in (x + 1)..nums.len() {
             let num1 = nums.get(x).unwrap();
             let num2 = nums.get(y).unwrap();
             let solution_1 = num1 + num2;
@@ -61,7 +56,7 @@ fn loop_solution() {
             }
             // Try to get solution 2 only if the sum of the current numbers is below 2020
             if solution_1 < YEAR {
-                for z in (y+1)..nums.len() {
+                for z in (y + 1)..nums.len() {
                     let num3 = nums.get(z).unwrap();
                     let solution_2 = solution_1 + num3;
                     if solution_2 == YEAR {
