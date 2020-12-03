eb0d12877f4ad0cc4df4d6e473175c102a1c0119
Kabir Sala <kabisala@amazon.com>
2020-12-03 11:45:58 +0000

    Code cleanup

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -5,39 +5,29 @@ fn main() {
 }
 
 fn solution() {
-    let mut lines: Vec<&str> = vec![];
     let content = fs::read_to_string("src/input.txt").expect("could not read file");
-    for row in content.lines() {
-        lines.push(row);
-    }
+    let board = content.lines().collect::<Vec<&str>>();
 
-    let input_width = lines[0].len();
-    let moves = vec![(3, 1), (1, 1), (5, 1), (7, 1), (1, 2)];
-    let mut x;
-    let mut y;
-    let mut tree_found;
-    let mut first_solution = 0;
-    let mut total: i64 = 1; // Set to 1 so we can just multiply it with the result
-    for (right, down) in &moves {
-        tree_found = 0;
-        x = 0;
-        y = 0;
-        loop {
-            y = (y + right) % input_width; // Modulo so we wrap around the field
-            x = x + down;
-            if x >= lines.len() {
-                // Save the result of the first set of moves
-                if first_solution == 0 {
-                    first_solution = tree_found;
-                }
-                break;
-            }
-            if lines[x].chars().nth(y).unwrap() == '#' {
-                tree_found = tree_found + 1;
-            }
-        }
-        total = total * tree_found;
+    
+    let solution_1 = calculate_downslope(&board, 3, 1);
+    println!("First solution: {}", solution_1);
+
+    let moves = vec![(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)];
+    let mut solution_2: i64 = 1;
+    for (right, down) in moves {
+        solution_2 *= calculate_downslope(&board, right, down) as i64;
     }
-    println!("First solution: {}", first_solution);
-    println!("Second solution: {}", total);
+    println!("Second solution: {}", solution_2);
+}
+
+fn calculate_downslope(board: &Vec<&str>, right: usize, down: usize) -> i32 {
+    let mut y = 0;
+    let mut tree_found = 0;
+    for line in board.iter().skip(down).step_by(down) {
+        y = (y + right) % line.len();
+        if line.chars().nth(y).unwrap() == '#' {
+            tree_found += 1;
+        }
+    }
+    tree_found
 }
